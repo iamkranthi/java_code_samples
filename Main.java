069662875c0e0a89237c66d54cf4d9048325b4cf
kranthi <kranthigowthamkumar@gmail.com>
2022-08-16 10:30:46 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+    //System.out.println("noobie");
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    System.out.println(num);
+    in.close();
+}
+
+}
